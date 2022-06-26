cf4484af2e2422baa36cfe31e4fec730f942ae1d
Akash Pal <creativeakashpal@gmail.com>
2022-06-26 20:26:58 +0530

    added display controller

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-(function () {
-    const gameBoard = {
-        gameBoardArray : [...Array(3)].map(e => Array(3).fill('')),
-    }
-
-    //player symbol altering
-
-    const blockPos = document.querySelectorAll('[data-position]');
-    blockPos.forEach(block => {
-        block.addEventListener('click', () => {
-            let positionX = block.dataset.position.split(',')[0];
-            let positionY = block.dataset.position.split(',')[1];
-
-            console.log(block.dataset.position.split(',')[1])
-
-            gameBoard.gameBoardArray[positionX][positionY]= 0
-            console.log(gameBoard.gameBoardArray)
-           
-            // changePlayer()--> changes player and its sign everytime the block is clicked
-            
-        })
-    })
-    
-    // console.log(gameBoard.gameBoardArray)
-
-})();
-
 function createPlayer(name, playerChoice) {
     return {
         name,
@@ -34,3 +7,56 @@ function createPlayer(name, playerChoice) {
 
 const player1 = createPlayer('player1', '0')
 const Player2 = createPlayer('player2', 'X') 
+
+const game = (function () {
+    const gameBoard = {
+        gameBoardArray : [...Array(3)].map(e => Array(3).fill('')),
+    }
+
+    return {
+        gameboard : gameBoard.gameBoardArray
+    }
+
+
+})();
+
+const displayController = (function() {
+    playerSign = player1.playerChoice;
+
+    const blockPos = document.querySelectorAll('[data-position]');
+    blockPos.forEach(block => {
+        block.addEventListener('click', updateGameboardArray)
+        block.addEventListener('click', updateGameboard)
+    });
+
+    function updateGameboardArray() {
+        let positionX = this.dataset.position.split(',')[0];
+        let positionY = this.dataset.position.split(',')[1];
+
+        if(game.gameboard[positionX][positionY] === '') {
+            game.gameboard[positionX][positionY] = playerSign;
+            console.log(game.gameboard);
+        }
+        
+    }
+
+    function updateGameboard() {
+        if(this.textContent === '') {
+            this.textContent = playerSign;
+            changePlayer();
+        }   
+    }
+
+    function changePlayer() {
+        if(playerSign === player1.playerChoice) {
+            playerSign = Player2.playerChoice
+        }
+        else {
+            playerSign = player1.playerChoice
+        }
+    }
+
+    
+
+})()
+

dbca74f3bed6cb06c24ab54675ba7671c9b3b08a
Akash Pal <creativeakashpal@gmail.com>
2022-06-28 20:00:45 +0530

    restructured isGameOver function

--- script.js
+++ script.js
@@ -13,41 +13,7 @@ const game = (function () {
         gameBoardArray : [...Array(3)].map(e => Array(3).fill('')),
     }
 
-    return {
-        gameboard : gameBoard.gameBoardArray
-    }
-
-})();
-
-const displayController = (function() {
     let playerSign = player1.playerChoice;
-
-
-    const blockPos = document.querySelectorAll('[data-position]');
-    blockPos.forEach(block => {
-        block.addEventListener('click', updateGameboardArray)
-        block.addEventListener('click', updateGameboard)
-    });
-
-    function updateGameboardArray() {
-        let positionX = this.dataset.position.split(',')[0];
-        let positionY = this.dataset.position.split(',')[1];
-
-        if(game.gameboard[positionX][positionY] === '') {
-            game.gameboard[positionX][positionY] = playerSign;
-        }
-        isGameOver(positionX, positionY).winnerCheck();
-        if(isGameOver(positionX, positionY).checkDraw()){
-            console.log('draw')
-        }
-    }
-
-    function updateGameboard() {
-        if(this.textContent === '') {
-            this.querySelector('h1').textContent = playerSign;
-            changePlayer();
-        }   
-    }
 
     function changePlayer() {
         if(playerSign === player1.playerChoice) {
@@ -57,32 +23,108 @@ const displayController = (function() {
             playerSign = player1.playerChoice
         }
     }
-    
-})()
 
-const isGameOver = function(positionX, positionY) {
+    const getSign = function() {
+        return playerSign;
+    }
+
+    return {
+        gameboard : gameBoard.gameBoardArray,
+        changePlayer,
+        getSign
+    }
+
+})();
+
+const displayController = (function() {
+    // let playerSign = player1.playerChoice;
+
+
+    const blockPos = document.querySelectorAll('[data-position]');
+    blockPos.forEach(block => {
+        block.addEventListener('click', updateGameboardArray);
+        block.addEventListener('click', updateGameboard);
+        block.addEventListener('click', game.changePlayer);
+    });
+
+    function updateGameboardArray() {
+        let positionX = this.dataset.position.split(',')[0];
+        let positionY = this.dataset.position.split(',')[1];
+
+        if(game.gameboard[positionX][positionY] === '') {
+            game.gameboard[positionX][positionY] = game.getSign();
+        }
+        // isGameOver(positionX, positionY).winnerCheck();
+        if(isGameOver.checkDraw()){
+            console.log('draw')
+        }
+    }
+
+    function updateGameboard() {
+        if(this.textContent === '') {
+            this.querySelector('h1').textContent = game.getSign();
+            // changePlayer();
+        }   
+    }
+    
+})();
+
+const isGameOver = (function() {
     const board = game.gameboard;
+    let winner;
 
     function winnerCheck() {
-        if(columnCheck(board) || diagonalCheck(board) || rowCheck(board)) {
-            console.log('winner')
+        if(columnCheck() || diagonalCheck() || rowCheck()) {
+            console.log('winner');
+            winner = game.getSign();
+            return true;
         }
     }
 
     function columnCheck() {
-        let column = [board[0][positionY], board[1][positionY], board[2][positionY]]
+
+    
+        for(let row=0; row<3; row++) {
+            let column = [];
+            for(let col=0; col<3; col++) {
+                column.push(board[col][row])
+            }
+            if (column.every(item => item === 'X') || 
+                column.every(item => item === '0')) {
+                    return true;
+            }
+        }
+   
+
+        /* let column = [board[0][positionY], board[1][positionY], board[2][positionY]]
         if (column.every(item => item === 'X') || 
             column.every(item => item === '0')) {
                 return true;
-        }
+        } */
     }
 
     function rowCheck() {
-        let row = [board[positionX][0], board[positionX][1], board[positionX][2]]
-        if (row.every(item => item === 'X') || 
-            row.every(item => item === '0')) {
+
+        
+        for(let row=0; row<3; row++) {
+            if (board[row].every(item => item === 'X') || 
+                board[row].every(item => item === '0')) {
                 return true;
+            }
         }
+       
+
+        // board.forEach(item => {
+        //     if (item.every(e => e === 'X') || 
+        //         item.every(e => e === '0')) {
+        //         return true;
+        //     }
+        // })
+        // let row = [board[positionX][0], board[positionX][1], board[positionX][2]]
+        // if (row.every(item => item === 'X') || 
+        //     row.every(item => item === '0')) {
+        //         return true;
+        // }
     }
 
     function diagonalCheck() {
@@ -115,7 +157,13 @@ const isGameOver = function(positionX, positionY) {
         winnerCheck,
         checkDraw
     }
-}
+})();
+
+// const showResult = function() {
+
+// }
+
+
 
 const button = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
